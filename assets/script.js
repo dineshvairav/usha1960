f098477cee228420fcbc44b672c008abd1d59acd
dineshvairav <ushaagency1960@gmail.com>
2025-06-01 17:38:04 +0530

    Neat the section

--- assets/script.js
+++ assets/script.js
@@ -21,33 +21,7 @@ const hamburger = document.querySelector('.hamburger');
   navMenu.classList.remove('active');
   }
  });
- 
 
- const wrapper = document.querySelector('.wrapper');
- const loginLink = document.querySelector('.login-link');
- const registerLink = document.querySelector('.register-link');
- const btnPopup = document.querySelector('.btn-sign');
- const iconClose = document.querySelector('.icon-close');
- 
-
- registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
- });
- 
-
- loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
- });
- 
-
- btnPopup.addEventListener('click', () => {
-  wrapper.classList.add('active-popup');
-  const popupLine = document.createElement('div');
-  popupLine.classList.add('popup-line');
-  popupLine.textContent = 'This is a popup line!';
-  document.body.appendChild(popupLine);
- });
- 
 
  iconClose.addEventListener('click', () => {
   wrapper.classList.remove('active-popup');
